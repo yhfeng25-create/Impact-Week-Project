e6496e9423da6765fb4a7044a632402363543779
Yihang <yhfeng25@gmail.com>
2026-03-23 12:18:32 +0800

    added homepage backend

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+from flask import Flask
+
+# 创建应用实例
+app = Flask(__name__)
+
+# 首页路由
+@app.route('/')
+def home():
+    return "✅ 我的第一个动态网站运行成功！Impact Week Project"
+
+# 第二个页面
+@app.route('/about')
+def about():
+    return "👋 这是关于页面"
+
+# 运行程序
+if __name__ == '__main__':
+    app.run(debug=True)
